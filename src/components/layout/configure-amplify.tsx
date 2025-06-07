fddb25f6dcb25231f9e9c4ef6bba243a0429dabf
matt-wigg <62004884+matt-wigg@users.noreply.github.com>
2025-06-07 16:05:25 -0500

    import type props with children to amplify configure

--- src/components/layout/configure-amplify.tsx
+++ src/components/layout/configure-amplify.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { PropsWithChildren } from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import outputs from "@infrastructure/amplify_outputs.json";
+import type { PropsWithChildren } from "react";
 
 /**
  * Configures the Amplify client runtime and wraps app in Authenticator context.
