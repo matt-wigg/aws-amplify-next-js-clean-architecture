70c17dbeff48bd8374c61296943846ab32074952
matt-wigg <62004884+matt-wigg@users.noreply.github.com>
2025-06-07 16:04:21 -0500

    move amplify configure out of use effect

--- src/components/layout/configure-amplify.tsx
+++ src/components/layout/configure-amplify.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import outputs from "@infrastructure/amplify_outputs.json";
@@ -8,10 +8,8 @@ import outputs from "@infrastructure/amplify_outputs.json";
 /**
  * Configures the Amplify client runtime and wraps app in Authenticator context.
  */
-export function ConfigureAmplifyClientSide({ children }: PropsWithChildren) {
-  useEffect(() => {
-    Amplify.configure(outputs, { ssr: true });
-  }, []);
+Amplify.configure(outputs, { ssr: true });
 
+export function ConfigureAmplifyClientSide({ children }: PropsWithChildren) {
   return <Authenticator.Provider>{children}</Authenticator.Provider>;
 }
